c0c9fdae572885ca18d0c90b2cf40d99f00a1895
GarrettSmithyNL <garrett_smith_30@hotmail.com>
2024-06-10 18:58:56 -0230

    Added event logging to disk

--- emitter.js
+++ emitter.js
@@ -1,4 +1,7 @@
 const EventEmitter = require("events");
+const path = require("path");
+const fs = require("fs");
+const fsPromise = require("fs").promises;
 
 class MyEmitter extends EventEmitter {}
 const myEmitter = new MyEmitter();
@@ -7,18 +10,56 @@ const { format, getYear } = require("date-fns");
 const { v4: uuid } = require("uuid");
 
 if (myEmitter.listenerCount("event") === 0) {
-  myEmitter.on("event", (event, message) => {
+  myEmitter.on("event", async (event, message) => {
     const dateTime = `${format(new Date(), "yyyyMMdd HH:mm:ss")}`;
     const logItem = `${dateTime} - ${event} - ${message} - ${uuid()}`;
-    console.log(logItem);
+    try {
+      const currentFolder = "logs/" + getYear(new Date());
+      if (!fs.existsSync(path.join(__dirname, "logs/"))) {
+        await fsPromise.mkdir(path.join(__dirname, "logs/"));
+        if (!fs.existsSync(path.join(__dirname, currentFolder))) {
+          await fsPromise.mkdir(path.join(__dirname, currentFolder));
+        }
+      } else {
+        if (!fs.existsSync(path.join(__dirname, currentFolder))) {
+          await fsPromise.mkdir(path.join(__dirname, currentFolder));
+        }
+      }
+      const fileName = `${format(new Date(), "yyyyMMdd")}` + "_http_events.log";
+      await fsPromise.appendFile(
+        path.join(__dirname, currentFolder, fileName),
+        logItem + "\n"
+      );
+    } catch (err) {
+      myEmitter.emit("error", "ERROR", "Folder or File already created");
+    }
   });
 }
 
 if (myEmitter.listenerCount("error") === 0) {
-  myEmitter.on("error", (event, message) => {
+  myEmitter.on("error", async (event, message) => {
     const dateTime = `${format(new Date(), "yyyyMMdd HH:mm:ss")}`;
     const logItem = `${dateTime} - ${event} - ${message} - ${uuid()}`;
-    console.log(logItem);
+    try {
+      const currentFolder = "logs/" + getYear(new Date());
+      if (!fs.existsSync(path.join(__dirname, "logs/"))) {
+        await fsPromise.mkdir(path.join(__dirname, "logs/"));
+        if (!fs.existsSync(path.join(__dirname, currentFolder))) {
+          await fsPromise.mkdir(path.join(__dirname, currentFolder));
+        }
+      } else {
+        if (!fs.existsSync(path.join(__dirname, currentFolder))) {
+          await fsPromise.mkdir(path.join(__dirname, currentFolder));
+        }
+      }
+      const fileName = `${format(new Date(), "yyyyMMdd")}` + "_http_events.log";
+      await fsPromise.appendFile(
+        path.join(__dirname, currentFolder, fileName),
+        logItem + "\n"
+      );
+    } catch (err) {
+      myEmitter.emit("error", "ERROR", "Folder or File already created");
+    }
   });
 }
 
